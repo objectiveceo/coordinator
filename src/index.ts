104bf06c19c6938919a56ccb8487f00f944c4f46
Grayson <grayson.hansard@gmail.com>
2021-01-25 10:55:23 -0500

    Setup linting

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ const app = express();
 const PORT = 8000;
 app.get('/', (req,res) => res.send('Hello, world!'));
 app.listen(PORT, () => {
+	// tslint:disable-next-line:no-console
 	console.log(`Open http://localhost:${PORT}`);
 });
