9863c151a4b2d11269da8573bfd0e79b0575d86f
Grayson <grayson.hansard@gmail.com>
2021-01-27 15:03:43 -0500

    Add simple endpoint for displaying basic information

---
+++ src/api/v1/info.ts
@@ -0,0 +1,16 @@
+import core from 'express'
+import process from 'process'
+
+export function register(app: core.Application) {
+	return app.get('/api/v1/info', info);
+}
+
+const startup = new Date()
+
+function info(request: core.Request, response: core.Response) {
+	response.send({
+		startup,
+		NODE_ENV: process.env.NODE_ENV || 'local',
+		BUILD_NUMBER: process.env.BUILD_NUMBER || 'local',
+	})
+}
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { register } from './api/v1/info'
+
 const app = express();
 const PORT = 8000;
 app.get('/', (req,res) => res.send('Hello, world!'));
@@ -6,3 +8,5 @@ app.listen(PORT, () => {
 	// tslint:disable-next-line:no-console
 	console.log(`Open http://localhost:${PORT}`);
 });
+
+register(app)
