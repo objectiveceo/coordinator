1172b50b4e76c219acfbe26a4e2b738326827810
Grayson <grayson.hansard@gmail.com>
2021-01-28 13:01:00 -0500

    Pull posts from database and apply inline markdown rendering to title

--- src/blog/v1/index.ts
+++ src/blog/v1/index.ts
@@ -1,7 +1,9 @@
 import core from 'express'
+import MarkdownIt from 'markdown-it'
 import Mustache from 'mustache'
 import { Database } from 'sqlite3';
-import InMemoryBlog from '../../common/data/InMemoryBlog';
+
+const markdown = new MarkdownIt()
 
 export function register(app: core.Application, database: Database) {
 	return app.get('/', (req, res) => buildIndex(database, req, res))
@@ -13,10 +15,19 @@ function buildIndex(database: Database, request: core.Request, response: core.Re
 			throw error
 		}
 
-		const blog = new InMemoryBlog()
-		const posts = blog.fetchPosts()
-		const output = Mustache.render(row.template, { posts })
+		const template = row.template
 
-		response.send(output)
+		database.all('SELECT title, slug FROM posts ORDER BY date_created DESC;', (postError, rows) => {
+			if (postError) {
+				throw postError
+			}
+
+			const posts = rows.map(x => ({
+				slug: x.slug,
+				title: markdown.renderInline(x.title)}
+			))
+			const output = Mustache.render(row.template, { posts })
+			response.send(output)
+		})
 	})
 }
