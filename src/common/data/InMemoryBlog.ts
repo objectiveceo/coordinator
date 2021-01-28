14119e0ce4fc33577f461ffac52d2fec30c984bf
Grayson <grayson.hansard@gmail.com>
2021-01-27 22:39:35 -0500

    Make public display slightly more palatable

--- src/common/data/InMemoryBlog.ts
+++ src/common/data/InMemoryBlog.ts
@@ -5,7 +5,6 @@ export default class InMemoryBlog implements Blog {
 	fetchPosts(): BlogPost[] {
 		return [
 			new InMemoryBlogPost(),
-			new InMemoryBlogPost(),
 		]
 	}
 }
--- src/common/data/InMemoryBlogPost.ts
+++ src/common/data/InMemoryBlogPost.ts
@@ -1,5 +1,5 @@
 import { BlogPost } from './index';
 
 export default class InMemoryBlogPost implements BlogPost {
-	get title(): string { return "in memory blog post test "; }
+	get title(): string { return "something is coming"; }
 }
