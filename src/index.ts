f6264b0f2f666737f6f11c885f6cccd8ee6bfe83
Grayson <grayson.hansard@gmail.com>
2021-01-27 22:39:06 -0500

    Delegate creating index to blog

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import express from 'express'
 import process from 'process'
-import { register } from './api/v1/info'
+import { register as infoRegister } from './api/v1/info'
+import { register as blogRegister } from './blog/v1/index'
 
 const app = express()
 const PORT = process.env.PORT || 8000
-app.get('/', (req,res) => res.send('Hello, world!'))
+
 app.listen(PORT, () => {
 	// tslint:disable-next-line:no-console
 	console.log(`Open http://localhost:${PORT}`)
 })
 
-register(app)
+infoRegister(app)
+blogRegister(app)
