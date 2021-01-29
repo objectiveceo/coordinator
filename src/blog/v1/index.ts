67c7ff04f8ff81c8e1c0897cbf24126bf79edcde
Grayson <grayson.hansard@gmail.com>
2021-01-28 22:57:26 -0500

    Use new type rather than direct database calls in blog views

--- src/blog/v1/index.ts
+++ src/blog/v1/index.ts
@@ -2,11 +2,12 @@ import core from 'express'
 import MarkdownIt from 'markdown-it'
 import Mustache from 'mustache'
 import { Database } from 'sqlite3';
+import { Blog } from '../../common/data';
 
 const markdown = new MarkdownIt()
 
-export function register(app: core.Application, database: Database) {
-	app.get('/', (req, res) => buildIndex(database, req, res))
+export function register(app: core.Application, database: Database, repository: Blog) {
+	app.get('/', (req, res) => buildIndex(database, repository, req, res))
 	app.get('/posts/:slug', (req, res) => buildPost(database, req, res))
 }
 
@@ -29,25 +30,18 @@ function buildPost(database: Database, request: core.Request, response: core.Res
 	})
 }
 
-function buildIndex(database: Database, request: core.Request, response: core.Response) {
+async function buildIndex(database: Database, repository: Blog, request: core.Request, response: core.Response) {
+	const posts = (await repository.fetchPosts()).map(x => ({
+		slug: x.slug,
+		title: markdown.renderInline(x.title)}
+	))
+
 	database.get('SELECT template FROM templates WHERE key = ?', 'front-page', (error, row) => {
 		if (error) {
 			throw error
 		}
 
-		const template = row.template
-
-		database.all('SELECT title, slug FROM posts ORDER BY date_created DESC;', (postError, rows) => {
-			if (postError) {
-				throw postError
-			}
-
-			const posts = rows.map(x => ({
-				slug: x.slug,
-				title: markdown.renderInline(x.title)}
-			))
-			const output = Mustache.render(row.template, { posts })
-			response.send(output)
-		})
+		const output = Mustache.render(row.template, { posts })
+		response.send(output)
 	})
 }
