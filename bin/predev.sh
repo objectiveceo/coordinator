fbbc900e4d29cd3f8e78f8261bfcbb753ead8b5a
Grayson <grayson.hansard@gmail.com>
2021-01-28 20:09:26 -0500

    Add function to configure nginx

--- bin/predev.sh
+++ bin/predev.sh
@@ -18,6 +18,27 @@ function searchForDatabasePath {
 	echo "DATABASE_PATH=$DATABASE_PATH" > .env
 }
 
+function configureNGINX {
+	echo "# Configuring NGINX (if necessary)"
+	local expectedContainerName="${OBJCEO_NGINX_CONTAINER_NAME:-objectiveceo_nginx}"
+
+	local containers="$(docker container ls --all --filter "name=${expectedContainerName}")"
+	local lineCount="$(echo "${containers}" | wc -l)"
+	if [[ $lineCount -eq 1 ]]; then
+		echo "Creating container named ${expectedContainerName}"
+		docker create --name "$expectedContainerName" --publish 80:80 nginx:alpine
+	fi
+
+	docker ps --filter status=running | grep "$expectedContainerName" > /dev/null
+	if [[ $? -ne 0 ]]; then
+		echo "Copying conf file into ${expectedContainerName}"
+		docker cp nginx/objectiveceo.local.conf "$expectedContainerName":/etc/nginx/conf.d/
+
+		echo "Starting ${expectedContainerName}"
+		docker start "$expectedContainerName" > /dev/null
+	fi
+}
+
 function main {
 	## Assume having a .git, src, and nginx is good enough
 	if [[ ! -d .git && ! -d src && ! -d nginx ]]; then
@@ -26,6 +47,7 @@ function main {
 	fi
 
 	searchForDatabasePath
+	configureNGINX
 }
 
 main "$@"
