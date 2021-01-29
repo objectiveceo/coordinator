dc4aa0340c4723ad943361de8a3e4cfd60a4b82f
Grayson <grayson.hansard@gmail.com>
2021-01-28 22:41:02 -0500

    Update usages of BlogPost

--- src/common/data/DatabaseBlogRepository.ts
+++ src/common/data/DatabaseBlogRepository.ts
@@ -1,5 +1,6 @@
 import { Database } from "sqlite3";
 import { Blog, BlogPost } from ".";
+import { BlogPostBuilder } from "./BlogPostBuilder";
 
 export default class DatabaseBlogRepository implements Blog {
 	database: Database
@@ -10,13 +11,18 @@ export default class DatabaseBlogRepository implements Blog {
 
 	fetchPosts(): Promise<BlogPost[]> {
 		return new Promise( (resolve, reject) => {
-			this.database.all('SELECT title FROM posts ORDER BY date_created DESC;', (error, rows) => {
+			this.database.all('SELECT title, slug FROM posts ORDER BY date_created DESC;', (error, rows) => {
 				if (error) {
 					reject(error)
 					return
 				}
 
-				const posts = rows.map((row: { title: string; }) => new BlogPost(row.title))
+				const posts = rows.map((row: { title: string; slug: string }) => {
+					const builder = new BlogPostBuilder({})
+						.setTitle(row.title)
+						.setSlug(row.slug)
+					return BlogPost.from(builder.data)
+				})
 				resolve(posts)
 			})
 		})
--- src/common/data/__tests__/demo.ts
+++
@@ -1,6 +0,0 @@
-import InMemoryBlog from '../InMemoryBlog'
-
-test('blog test', () => {
-	let blog = new InMemoryBlog()
-	blog.fetchPosts().then( posts => expect(posts.length).toBe(1))
-})
