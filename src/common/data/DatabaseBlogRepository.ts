81469832f21f0d6f526f193878f2f025c383dc96
Grayson <grayson.hansard@gmail.com>
2021-01-30 23:26:43 -0500

    Convert string to date
    Well, so much for guaranteed type safety...

--- src/common/data/DatabaseBlogRepository.ts
+++ src/common/data/DatabaseBlogRepository.ts
@@ -26,7 +26,7 @@ export default class DatabaseBlogRepository implements Blog {
 					.setSlug(slug)
 					.setTitle(row.title)
 					.setContent(row.contents)
-					.setCreationDate(row.creationDate)
+					.setCreationDate(new Date(`${row.date_created} UTC`))
 
 				resolve(BlogPost.from(builder.data))
 			})
