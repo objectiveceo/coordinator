5d8386f850ba6f2a646baeb720fb129f97628371
Grayson <grayson.hansard@gmail.com>
2021-02-18 22:39:18 -0500

    Update tests to recognize JWTs

--- src/api/v1/user/__tests__/index.ts
+++ src/api/v1/user/__tests__/index.ts
@@ -1,6 +1,8 @@
 import express from 'express'
+import jsonwebtoken from 'jsonwebtoken'
 import request from 'supertest'
 import { register } from '../'
+import JWTSeedProvider from '../util/jwtseedprovider'
 import User from '../util/user'
 import UserStorage, { CreateParams, VerifyResult, VerifyStatus } from '../util/userstorage'
 
@@ -27,12 +29,45 @@ class TestUserStorage implements UserStorage {
 	}
 }
 
+class TestSeedProvider implements JWTSeedProvider {
+	accessTokenSeed: Promise<string> | null
+	refreshTokenSeed: Promise<string> | null
+
+	constructor() {
+		this.accessTokenSeed = Promise.resolve('access')
+		this.refreshTokenSeed = Promise.resolve('refresh')
+	}
+
+	generateAccessTokenSeed(user: User): Promise<string> {
+		return this.accessTokenSeed!
+	}
+	generateRefreshTokenSeed(user: User): Promise<string> {
+		return this.refreshTokenSeed!
+	}
+	revokeAccessToken(user: User): Promise<void> {
+		this.accessTokenSeed = null
+		return Promise.resolve()
+	}
+	revokeRefreshToken(user: User): Promise<void> {
+		this.refreshTokenSeed = null
+		return Promise.resolve()
+	}
+}
+
 describe('/api/v1/user tests', () => {
 	const app = express()
 	app.use(express.urlencoded())
 
+	const seedProvider = new TestSeedProvider
+	const expirationGenerator = () => {
+		return {
+			access: { date: new Date, expiresIn: '15m' },
+			refresh: { date: new Date, expiresIn: '15m' },
+		}
+	}
+
 	const storage = new TestUserStorage()
-	register(app, storage)
+	register(app, storage, seedProvider, expirationGenerator)
 
 	test('GET ./', async () => {
 		const result = await request(app)
@@ -48,9 +83,15 @@ describe('/api/v1/user tests', () => {
 			.send('name=test&password=pword')
 		
 		expect(result.status).toBe(200)
-		expect(result.body.status).toBe(1)
-		expect(result.body.user).not.toBeNull()
-		expect(result.body.user.name).toBe('test')
-		expect(result.body.user.email).toBe('test@test.com')
+
+		expect(result.body.accessToken).not.toBeNull()
+		const accessPayload = jsonwebtoken.verify(result.body.accessToken, await seedProvider.accessTokenSeed!) as User
+		expect(accessPayload.name).toBe('test')
+		expect(accessPayload.email).toBe('test@test.com')
+
+		expect(result.body.refreshToken).not.toBeNull()
+		const refreshPayload = jsonwebtoken.verify(result.body.refreshToken, await seedProvider.refreshTokenSeed!) as User
+		expect(refreshPayload.name).toBe('test')
+		expect(refreshPayload.email).toBe('test@test.com')
 	})
 })
